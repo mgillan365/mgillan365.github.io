a097f1bb1488e7b1d45d3bdbc491a760f7e7a07b
mgillan365 <mgillan@me.com>
2026-06-09 21:46:33 -0700

    new commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const rockImg = document.querySelector('#rockImg');
 const defaultRock = 'rock.png';
 const happyRock = 'rock_happy.png';
 const rockResetDelay = 3 * 60 * 1000;
-const rockTopOffset = 100;
 
 let userName = '';
 let resetTimerId;
@@ -11,14 +10,8 @@ const greetUser = () => {
   alert('Hello, I am your pet rock.');
 };
 
-const resizeRock = () => {
-  const availableHeight = window.innerHeight - rockTopOffset;
-  rockImg.style.height = `${availableHeight * 0.9}px`;
-};
-
 const resetRock = () => {
   rockImg.src = defaultRock;
-  resizeRock();
 };
 
 const touchRock = () => {
@@ -33,14 +26,10 @@ const touchRock = () => {
   }
 
   rockImg.src = happyRock;
-  resizeRock();
 
   clearTimeout(resetTimerId);
   resetTimerId = setTimeout(resetRock, rockResetDelay);
 };
 
 rockImg.addEventListener('click', touchRock);
-rockImg.addEventListener('load', resizeRock);
 window.addEventListener('load', greetUser);
-window.addEventListener('load', resizeRock);
-window.addEventListener('resize', resizeRock);
